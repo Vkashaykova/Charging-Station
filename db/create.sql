3075dc7cde6ca30070cf8c31ba409ec3b51eda00
Vkashaykova <vkashaykova@gmail.com>
2024-05-03 14:17:30 +0300

    commit data base

---
+++ db/create.sql
@@ -0,0 +1,20 @@
+create table zipcode
+(
+    zipcode_id int not null
+        primary key,
+    zipcode    int not null
+);
+
+create table charging_station
+(
+    id        varchar(50)    not null
+        primary key,
+    latitude  decimal(10, 6) not null,
+    longitude decimal(10, 6) not null,
+    zipcode   int            not null,
+    constraint charging_station_pk_2
+        unique (id),
+    constraint charging_station_zipcode_zipcode_id_fk
+        foreign key (zipcode) references zipcode (zipcode_id)
+);
+
